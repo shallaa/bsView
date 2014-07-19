cb8f2cb1c86316789a9ebbb514a2dd6c38196a43
oopchoi <oopchoi@gmail.com>
2014-07-19 14:21:12 +0900

    test

---
+++ DOM/test/oopchoi/oopchoiView.js
@@ -0,0 +1,59 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 14. 7. 19
+ * Time: 오후 12:56
+ * To change this template use File | Settings | File Templates.
+ */
+bs.cls('Button', function (fn, clsfn, bs) {
+    fn.NEW = function (key) {
+        this.ID = Math.floor(Math.random() * 10);
+        this.label;
+        this.width;
+        this.height;
+        this.fillStyle;
+        this.x;
+        this.y;
+        this.rotation;
+        this.alpha;
+        this.fontSize;
+        this.fontFamily;
+        this.lineWidth;
+        this.strokeStyle;
+
+        this.context;
+    }
+
+    fn.destroy = function () {
+        this.END();
+    }
+
+    // 그리기
+    fn.draw = function(){
+        this.context.beginPath();
+        this.context.rect(this.x, this.y, this.width, this.height);
+        this.context.fillStyle = this.fillStyle;
+        this.context.fill();
+        this.context.lineWidth = this.lineWidth;
+        this.context.strokeStyle = this.strokeStyle;
+        this.context.stroke();
+        this.context.font = this.fontSize + 'pt ' + this.fontFamily;
+        this.context.fillText(this.label, this.width, this.height);
+    }
+
+    fn.addEventListener = function(type, handler){
+
+    }
+
+    fn.removeEventListener = function(type, handler){
+
+    }
+
+    fn.post = function(){
+
+    }
+
+
+
+
+})
