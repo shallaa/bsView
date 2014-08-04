2d369c6ba3efa72afed6d65314b3c46a67cf8cca
hikaMaeng <hika00@gmail.com>
2014-08-05 00:39:43 +0900

    init 분리

--- DOM/bsUI.0.1.js
+++ DOM/bsUI.0.1.js
@@ -1,19 +1,10 @@
-module.exports.ui = {
+module.exports.ui = (function(){
+var inited, init = function(){
+	inited = 1,
+	bs.Css('#bsUI_toast').S( 'overflow', 'hidden', 'position', 'fixed', tb, 0, 'background', '#2b2b2f', 'z-index', 999999999 );
+}, ui = {
 	toast:function(){
-		var r = bs.rand, rf = bs.randf, tb = bs.DETECT.device == 'mobile' ? 'bottom' : 'top',
-		particle = function( target, rate, T ){
-			var i = r( 2, 5 ), x, y, s, d;
-			while(i--) bs.ANI.style(
-				bs.Dom('<div></div>' ).S( 'background', 'rgb('+r(100,200)+','+r(100,200)+','+r(100,200)+')', 'border-radius', 100, 'position','fixed', '<', 'body',
-					'display', 'block', 'width',10, 'height',10, 'left', ( x = ( 1 + ( i % 2 ? -rate : rate ) ) * 50 ) + ':%', tb, y = bs.rand( 10, 40 ), 'opacity', 1, 'this'
-				),
-				'left', r( -10, 10 ) + x, 'top', r( -50, 100 ) + y,
-				'width', s = r( 30, 60 ), 'height',s, 'opacity', 0, 
-				'time', rf( .5, 1.5 ), 'end', particleEnd
-			);
-		},
-		particleEnd = function(target){target.S(null);},
-		toast = function( msg, time, durationTime, ease, parent ){
+		var toast = function( msg, time, durationTime, ease, parent ){
 			if( msg ) toastQue.push(arguments);
 			if( toasted || !toastQue.length ) return;
 			toasted = 1, curr = toastQue.pop();
@@ -30,9 +21,25 @@ module.exports.ui = {
 				'end', hide, 'update', particle
 			);
 		},
+		particle = function( target, rate, T ){
+			var i = r( 2, 5 ), x, y, s, d;
+			while(i--) bs.ANI.style(
+				bs.Dom('<div></div>' ).S( 'background', 'rgb('+r(100,200)+','+r(100,200)+','+r(100,200)+')', 'border-radius', 100, 'position','fixed', '<', 'body',
+					'display', 'block', 'width',10, 'height',10, 'left', ( x = ( 1 + ( i % 2 ? -rate : rate ) ) * 50 ) + ':%', tb, y = bs.rand( 10, 40 ), 'opacity', 1, 'this'
+				),
+				'left', r( -10, 10 ) + x, 'top', r( -50, 100 ) + y,
+				'width', s = r( 30, 60 ), 'height',s, 'opacity', 0, 
+				'time', rf( .5, 1.5 ), 'end', particleEnd
+			);
+		},
+		particleEnd = function(target){target.S(null);},
 		hide = function(){dom.S( 'border-radius', 100, 'opacity', 0, 'width', 0, 'left', 50, 'display', 'none' ), toasted = 0, toast();},
+		r = bs.rand, rf = bs.randf, tb = bs.DETECT.device == 'mobile' ? 'bottom' : 'top',
 		toasted, toastQue = [], dom, curr;
-		bs.Css('#bsUI_toast').S( 'overflow', 'hidden', 'position', 'fixed', tb, 0, 'background', '#2b2b2f', 'z-index', 999999999 );
+		
+		if( !inited ) init();
 		(this.toast = toast).apply( this, arguments );
 	}
 };
+return ui;
+})();
